e29d9aeb38eadad335fc2c79142b697b8d0f3e67
Özer, Kaan <kaan.oezer@study.thws.de>
2023-07-06 21:49:46 +0200

    Event model added

--- src/main/java/de/fhws/fiw/fds/exam03/server/api/models/Event.java
+++ src/main/java/de/fhws/fiw/fds/exam03/server/api/models/Event.java
@@ -18,4 +18,98 @@ import de.fhws.fiw.fds.sutton.server.api.models.AbstractModel;
 
 public class Event extends AbstractModel
 {
+
+
+
+    private String topicShort;
+    private String topicLong;
+    private String address;
+    private String startDateAndTime;
+    private String endDateAndTime;
+    private String institution;
+    private String lecturer;
+    private String location;
+
+
+
+    public Event() {
+
+    }
+
+    public Event( String topicShort, String topicLong, String address, String startDateAndTime, String endDateAndTime, String institution, String lecturer, String location) {
+
+        this.topicShort = topicShort;
+        this.topicLong = topicLong;
+        this.address = address;
+        this.startDateAndTime = startDateAndTime;
+        this.endDateAndTime = endDateAndTime;
+        this.institution = institution;
+        this.lecturer = lecturer;
+        this.location = location;
+    }
+
+
+    public String getTopicShort() {
+        return topicShort;
+    }
+
+    public void setTopicShort(String topicShort) {
+        this.topicShort = topicShort;
+    }
+
+    public String getTopicLong() {
+        return topicLong;
+    }
+
+    public void setTopicLong(String topicLong) {
+        this.topicLong = topicLong;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStartDateAndTime() {
+        return startDateAndTime;
+    }
+
+    public void setStartDateAndTime(String startDateAndTime) {
+        this.startDateAndTime = startDateAndTime;
+    }
+
+    public String getEndDateAndTime() {
+        return endDateAndTime;
+    }
+
+    public void setEndDateAndTime(String endDateAndTime) {
+        this.endDateAndTime = endDateAndTime;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
