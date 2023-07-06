90aab3e252944ca37f7dffa58678365154aa8222
Özer, Kaan <kaan.oezer@study.thws.de>
2023-07-06 22:11:25 +0200

    EventDaoHibernateImpl changed for readById

--- src/main/java/de/fhws/fiw/fds/exam03/server/database/hibernate/dao/EventDaoHibernateImpl.java
+++ src/main/java/de/fhws/fiw/fds/exam03/server/database/hibernate/dao/EventDaoHibernateImpl.java
@@ -1,12 +1,16 @@
 package de.fhws.fiw.fds.exam03.server.database.hibernate.dao;
 
 import de.fhws.fiw.fds.exam03.server.database.hibernate.models.EventDB;
+import de.fhws.fiw.fds.exam03.server.database.hibernate.operations.LoadEventById;
+import de.fhws.fiw.fds.exam03.server.database.hibernate.operations.PersistEventOperation;
 import de.fhws.fiw.fds.sutton.server.database.SearchParameter;
 import de.fhws.fiw.fds.sutton.server.database.hibernate.results.CollectionModelHibernateResult;
 import de.fhws.fiw.fds.sutton.server.database.hibernate.results.SingleModelHibernateResult;
 import de.fhws.fiw.fds.sutton.server.database.results.NoContentResult;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.util.stream.IntStream;
 
 public class EventDaoHibernateImpl implements EventDaoHibernate
 {
@@ -18,24 +22,40 @@ public class EventDaoHibernateImpl implements EventDaoHibernate
 		populateDatabase( );
 	}
 
+	private void populateDatabase( )
+	{
+		IntStream.range( 0, 100 ).forEach(i -> create( createEvent( ) ) );
+	}
 
 	//*********************CRUD OPERATIONS*********************
-	@Override public NoContentResult create( EventDB model )
+	public NoContentResult create( EventDB model )
 	{
-		return new NoContentResult();
+		return new PersistEventOperation( emf, model  ).start();
 	}
+
+	private EventDB createEvent() {
+		final EventDB returnValue = new EventDB();
+		returnValue.setTopicShort("Bond");
+		returnValue.setTopicLong("James Bond");
+		returnValue.setAddress("123 Main Street");
+		returnValue.setStartDateAndTime("2023-07-05 10:00");
+		returnValue.setEndDateAndTime("2023-07-05 12:00");
+		returnValue.setInstitution("MI6");
+		returnValue.setLecturer("James Bond");
+		returnValue.setLocation("London");
+		return returnValue;
+	}
+
 
 
 	@Override public SingleModelHibernateResult<EventDB> readById( long id )
 	{
-		return new SingleModelHibernateResult<>(  );
+		return new LoadEventById( emf, id ).start();
 	}
-
 	@Override public CollectionModelHibernateResult<EventDB> readAll( SearchParameter searchParameter )
 	{
 		return new CollectionModelHibernateResult<>(  );
 	}
-
 
 	@Override public NoContentResult update( EventDB model )
 	{
@@ -47,8 +67,8 @@ public class EventDaoHibernateImpl implements EventDaoHibernate
 		return new NoContentResult();
 	}
 
-	private void populateDatabase( )
-	{
-	}
+
 
 }
+
+
