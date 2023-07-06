074377417d38a6a2370f5caf5463648fe0916db0
Özer, Kaan <kaan.oezer@study.thws.de>
2023-07-06 21:55:19 +0200

    eventDB added to the models package that is the bottom of the database package

--- src/main/java/de/fhws/fiw/fds/exam03/server/database/hibernate/models/EventDB.java
+++ src/main/java/de/fhws/fiw/fds/exam03/server/database/hibernate/models/EventDB.java
@@ -1,11 +1,128 @@
 package de.fhws.fiw.fds.exam03.server.database.hibernate.models;
 
 import de.fhws.fiw.fds.sutton.server.database.hibernate.models.AbstractDBModel;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
+import jakarta.persistence.*;
+import java.time.LocalDate;
 @Entity
 @Table( name = "events" )
 public class EventDB extends AbstractDBModel
 {
+
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "topic_short")
+    private String topicShort;
+
+    @Column(name = "topic_long")
+    private String topicLong;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "start_date_and_time")
+    private String startDateAndTime;
+
+    @Column(name = "end_date_and_time")
+    private String endDateAndTime;
+
+    @Column(name = "institution")
+    private String institution;
+
+    @Column(name = "lecturer")
+    private String lecturer;
+
+    @Column(name = "location")
+    private String location;
+
+
+
+    public EventDB() {
+    }
+
+    public EventDB( String topicShort, String topicLong, String address, String startDateAndTime, String endDateAndTime, String institution, String lecturer, String location) {
+
+        this.topicShort = topicShort;
+        this.topicLong = topicLong;
+        this.address = address;
+        this.startDateAndTime = startDateAndTime;
+        this.endDateAndTime = endDateAndTime;
+        this.institution = institution;
+        this.lecturer = lecturer;
+        this.location = location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTopicShort() {
+        return topicShort;
+    }
+
+    public void setTopicShort(String topicShort) {
+        this.topicShort = topicShort;
+    }
+
+    public String getTopicLong() {
+        return topicLong;
+    }
+
+    public void setTopicLong(String topicLong) {
+        this.topicLong = topicLong;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStartDateAndTime() {
+        return startDateAndTime;
+    }
+
+    public void setStartDateAndTime(String startDateAndTime) {
+        this.startDateAndTime = startDateAndTime;
+    }
+
+    public String getEndDateAndTime() {
+        return endDateAndTime;
+    }
+
+    public void setEndDateAndTime(String endDateAndTime) {
+        this.endDateAndTime = endDateAndTime;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
 }
