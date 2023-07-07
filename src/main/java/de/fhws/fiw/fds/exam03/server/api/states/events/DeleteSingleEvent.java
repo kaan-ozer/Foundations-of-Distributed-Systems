87fa05b6c4a9741862195d0ab25c718d45f61b6b
Özer, Kaan <kaan.oezer@study.thws.de>
2023-07-07 12:48:32 +0200

    DeleteSingleEvent added

---
+++ src/main/java/de/fhws/fiw/fds/exam03/server/api/states/events/DeleteSingleEvent.java
@@ -0,0 +1,52 @@
+package de.fhws.fiw.fds.exam03.server.api.states.persons;
+
+import de.fhws.fiw.fds.sutton.server.api.states.AbstractState;
+import de.fhws.fiw.fds.sutton.server.api.states.delete.AbstractDeleteState;
+import de.fhws.fiw.fds.sutton.server.database.results.NoContentResult;
+import de.fhws.fiw.fds.sutton.server.database.results.SingleModelResult;
+import de.fhws.fiw.fds.exam03.server.database.DaoFactory;
+import de.fhws.fiw.fds.exam03.server.api.models.Event;
+
+public class DeleteSingleEvent extends AbstractDeleteState<Event>
+{
+    public DeleteSingleEvent( final Builder builder )
+    {
+        super( builder );
+    }
+
+    public static class Builder extends AbstractDeleteStateBuilder
+    {
+        @Override
+        public AbstractState build( )
+        {
+            return new DeleteSingleEvent( this );
+        }
+    }
+
+    @Override
+    protected void defineTransitionLinks( )
+    {
+    }
+
+
+    @Override
+    protected SingleModelResult<Event> loadModel( )
+    {
+        return DaoFactory.getInstance( ).getEventDao( ).readById( this.modelIdToDelete );
+    }
+
+    @Override
+    protected NoContentResult deleteModel( )
+    {
+        return DaoFactory.getInstance( ).getEventDao( ).delete( this.modelIdToDelete );
+    }
+
+
+    @Override
+    protected void authorizeRequest( )
+    {
+
+    }
+
+
+}
